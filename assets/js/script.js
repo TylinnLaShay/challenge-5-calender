b13c25b2a031f4758b0e9e2a47e13986bc339b49
TylinnLaShay <tylinnhall@gmail.com>
2021-07-19 01:54:43 -0700

    created function to save data of textareas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,50 @@
 var timeNow = moment().format('MMMM Do YYYY');
 $("#currentDay").html(timeNow);
-console.log(currentTime);
+console.log(timeNow);
+
+$(document).ready(function () {
+    $(".saveBtn").on("click", function () {
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+        // push local storage
+        localStorage.setItem(time, text);
+    })
+   
+    function getTime() {
+        //CURRENT TIME
+        var currentTime = moment().hour();
+
+        // for each time block
+        $(".time-block").each(function () {
+            var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
+
+            if (timeBlock < currentTime) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+            else if (timeBlock === currentTime) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+
+            }
+        })
+    }
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
+
+    getTime();
+})
